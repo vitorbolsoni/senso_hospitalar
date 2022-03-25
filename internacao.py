c2cbcc48f5d943a632036f4f885ebe583dfffed9
vitorbolsoni <vitorbolsoni@gmail.com>
2022-03-24 23:04:20 -0300

    Primeiro commit!

--- internacao.py
+++ internacao.py
@@ -35,8 +35,8 @@ app = App()
 app.configurar(2, '#7C8428')
 arquivo_logo = tk.PhotoImage(file='logo_unimed_init.png')
 
-# topo = Topo()
-# topo.logo(arquivo_logo)
-# topo.titulos('Unimed Noroeste Capixaba', 'Senso Hospitalar', 'Ocupação Diária dos Leitos')
+topo = Topo()
+topo.logo(arquivo_logo)
+topo.titulos('Unimed Noroeste Capixaba', 'Senso Hospitalar', 'Ocupação Diária dos Leitos')
 
 app.mainloop()
