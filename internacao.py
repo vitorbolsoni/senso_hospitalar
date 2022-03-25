ea06e8bfdf33014085f6a1bef8724973bd3bf22e
vitorbolsoni <vitorbolsoni@gmail.com>
2022-03-25 01:14:48 -0300

    Posicionamento.

--- internacao.py
+++ internacao.py
@@ -4,16 +4,13 @@ import tkinter as tk
 class App(tk.Frame):
     def __init__(self, master):
         super().__init__(master)
-        self.pack(expand=1)
-
+        self.pack()
 
 class Fundo(App):
     def __init__(self, master):
         super().__init__(master)
-        master.pack_propagate(flag=True)
-
-    def configurar(self, borda, cor_fundo):
-        tk.Frame(self, borderwidth=borda, background=cor_fundo)
+        master = tk.Frame(self, borderwidth=2, background='#7C8428',)
+        master.pack()
 
 
 class Topo(Fundo):
@@ -22,24 +19,22 @@ class Topo(Fundo):
 
     def titulos(self, nome_instituicao, descricao_titulo, texto_cabecalho):
         instituicao = tk.Label(text=nome_instituicao, foreground='#009', background='#dde')
-        instituicao.place(x='500', bordermode='inside')
+        instituicao.pack(side='top', anchor='center', fill='none', padx=2, pady=2)
         titulo = tk.Label(text=descricao_titulo, font=('Copperplate', 14), foreground='#009', background='#dde')
-        titulo.place(bordermode='outside')
+        titulo.pack(side='top', anchor='center', fill='none', padx=2, pady=2)
         cabecalho = tk.Label(text=texto_cabecalho, font=('Copperplate', 11), foreground='#009', background='#dde')
-        cabecalho.place(anchor='center')
+        cabecalho.pack(side='top', anchor='center', fill='none', padx=2, pady=2)
 
     def logo(self, arquivo):
         logo = tk.Label(image=arquivo, background='#dde')
-        logo.place(anchor='nw')
+        logo.pack(side='left', anchor='nw', fill='none', padx=2, pady=2)
 
 
 janela = tk.Tk()
 app = App(janela)
 arquivo_logo = tk.PhotoImage(file='logo_unimed_init.png')
-
-topo = Topo(app)
-topo.configurar(2, '#7C8428')
+fundo = Fundo(app)
+topo = Topo(fundo)
 topo.logo(arquivo_logo)
 topo.titulos('Unimed Noroeste Capixaba', 'Senso Hospitalar', 'Ocupação Diária dos Leitos')
-
 app.mainloop()
