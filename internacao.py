afb5100931e6342e53bdacdd8c7d5a273f181ca8
vitorbolsoni <vitorbolsoni@gmail.com>
2022-03-27 21:42:40 -0300

    27/03/2022

--- internacao.py
+++ internacao.py
@@ -1,71 +1,57 @@
 import tkinter as tk
 
 
-class App(tk.Frame):
-    def __init__(self, master=None):
+class JanelaInternacao(tk.Frame):
+    def __init__(self, master):
         super().__init__(master)
         self.pack()
 
+    def fundo(self, bg='#EB4E1C'):
+        fundo = tk.Frame(self, borderwidth=2, background=bg)
+        fundo.place(width=1000, height=1000)
 
-class Fundo(App):
-    def __init__(self, master):
-        super().__init__(master)
-        master = tk.Frame(self, borderwidth=2, background='#EB4E1C')
-        master.place(width=1000, height=1000)
+    def titulos(self, nome_instituicao, descricao_titulo, texto_cabecalho, py=2, bg='#dde'):
+        instituicao = tk.Label(self, text=nome_instituicao, foreground='#009', background=bg)
+        instituicao.pack(side='top', anchor='center', fill='none', padx=2, pady=py)
+        titulo = tk.Label(self, text=descricao_titulo, font=('Copperplate', 14), foreground='#009', background=bg)
+        titulo.pack(side='top', anchor='center', fill='none', padx=2, pady=py)
+        cabecalho = tk.Label(self, text=texto_cabecalho, font=('Copperplate', 11), foreground='#009', background=bg)
+        cabecalho.pack(side='top', anchor='center', fill='none', padx=2, pady=py)
 
+    def logo(self, arquivo, py=2, bg='#dde'):
+        logo = tk.Label(self, image=arquivo, background=bg)
+        logo.pack(side='left', anchor='nw', fill='none', padx=2, pady=py)
 
-class Topo(Fundo):
-    def __init__(self, master):
-        super().__init__(master)
-
-    def titulos(self, nome_instituicao, descricao_titulo, texto_cabecalho):
-        instituicao = tk.Label(self, text=nome_instituicao, foreground='#009', background='#dde')
-        instituicao.pack(side='top', anchor='center', fill='none', padx=2, pady=2)
-        titulo = tk.Label(self, text=descricao_titulo, font=('Copperplate', 14), foreground='#009', background='#dde')
-        titulo.pack(side='top', anchor='center', fill='none', padx=2, pady=2)
-        cabecalho = tk.Label(self, text=texto_cabecalho, font=('Copperplate', 11), foreground='#009', background='#dde')
-        cabecalho.pack(side='top', anchor='center', fill='none', padx=2, pady=2)
-
-    def logo(self, arquivo):
-        logo = tk.Label(self, image=arquivo, background='#dde')
-        logo.pack(side='left', anchor='nw', fill='none', padx=2, pady=2)
-
-    def status(self):
-        status = tk.Label(self, text='teste status', background='#dde')
+    def status(self, bg='#dde'):
+        status = tk.Label(self, text='teste status', background=bg)
         status.pack(side='right', anchor='ne', fill='none', padx=2, pady=2)
 
-
-class AbaJanela(Fundo):
-    def __init__(self, master):
-        super().__init__(master)
-
-    def aba(self):
-        ind_setor = tk.Label(self, text='Setores', foreground='#009')
-        ind_setor.pack(side='left', anchor='n', fill='none', padx=2, pady=2)
-        ind_leitos = tk.Label(self, text='Total de Leitos', foreground='#009')
-        ind_leitos.pack(side='left', anchor='n', fill='none', padx=2, pady=2)
-        ind_ocupacao = tk.Label(self, text='Ocupação', foreground='#009')
-        ind_ocupacao.pack(side='left', anchor='n', fill='none', padx=2, pady=2)
-        ind_perc_ocupacao = tk.Label(self, text='% Ocupação', foreground='#009')
-        ind_perc_ocupacao.pack(side='left', anchor='n', fill='none', padx=2, pady=2)
+    def tabela(self, py=20, cor_letra='#009'):
+        ind_setor = tk.Label(self, text='Setores', foreground=cor_letra)
+        ind_setor.pack(side='left', anchor='n', fill='none', padx=2, pady=py)
+        ind_leitos = tk.Label(self, text='Total de Leitos', foreground=cor_letra)
+        ind_leitos.pack(side='left', anchor='n', fill='none', padx=2, pady=py)
+        ind_ocupacao = tk.Label(self, text='Ocupação', foreground=cor_letra)
+        ind_ocupacao.pack(side='left', anchor='n', fill='none', padx=2, pady=py)
+        ind_perc_ocupacao = tk.Label(self, text='% Ocupação', foreground=cor_letra)
+        ind_perc_ocupacao.pack(side='left', anchor='n', fill='none', padx=2, pady=py)
         ind_altastransf = tk.Label(self, text='''Altas ou
-Transferências Internas''', foreground='#009')
-        ind_altastransf.pack(side='left', anchor='n', fill='none', padx=2, pady=2)
+Transferências Internas''', foreground=cor_letra)
+        ind_altastransf.pack(side='left', anchor='n', fill='none', padx=2, pady=py)
         ind_prev_alta = tk.Label(self, text='''Previsão de
-Alta Para Amanhã''', foreground='#009')
-        ind_prev_alta.pack(side='left', anchor='n', fill='none', padx=2, pady=2)
-        ind_obito = tk.Label(self, text='Óbitos', foreground='#009')
-        ind_obito.pack(side='left', anchor='n', fill='none', padx=2, pady=2)
+Alta Para Amanhã''', foreground=cor_letra)
+        ind_prev_alta.pack(side='left', anchor='n', fill='none', padx=2, pady=py)
+        ind_obito = tk.Label(self, text='Óbitos', foreground=cor_letra)
+        ind_obito.pack(side='left', anchor='n', fill='none', padx=2, pady=py)
 
 
-app = App()
-fundo = Fundo(app)
-topo = Topo(fundo)
-arquivo_logo = tk.PhotoImage(file='logo_unimed_init.png')
-topo.logo(arquivo_logo)
-topo.status()
-topo.titulos('Unimed Noroeste Capixaba', 'Senso Hospitalar', 'Ocupação Diária dos Leitos')
-clinica = AbaJanela(app)
-clinica.aba()
-
-app.mainloop()
+def abre_janela_internacao():
+    root = tk.Toplevel()
+    internacao = JanelaInternacao(root)
+    internacao.fundo()
+    arquivo_logo = tk.PhotoImage(file='logo_unimed_init.png')
+    internacao.logo(arquivo_logo)
+    internacao.status()
+    internacao.titulos('Unimed Noroeste Capixaba', 'Senso Hospitalar', 'Ocupação Diária dos Leitos')
+    internacao.tabela()
+    root.mainloop()
--- menu.py
+++ menu.py
@@ -1,8 +1,44 @@
 import tkinter as tk
-# from internacao import
+from internacao import abre_janela_internacao
 
 
-class ChamaInternacao(tk.Toplevel):
+class JanelaMenu(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
+        self.pack()
 
+    def fundo(self, bg='#EB4E1C'):
+        fundo = tk.Frame(self, borderwidth=2, background=bg)
+        fundo.place(width=1000, height=1000)
+
+    def titulos(self, nome_instituicao, descricao_titulo, texto_cabecalho, px=2, py=2, bg='#dde'):
+        instituicao = tk.Label(self, text=nome_instituicao, foreground='#009', background=bg)
+        instituicao.pack(side='top', anchor='center', fill='none', padx=px, pady=py)
+        titulo = tk.Label(self, text=descricao_titulo, font=('Copperplate', 14), foreground='#009', background=bg)
+        titulo.pack(side='top', anchor='center', fill='none', padx=px, pady=py)
+        cabecalho = tk.Label(self, text=texto_cabecalho, font=('Copperplate', 11), foreground='#009', background=bg)
+        cabecalho.pack(side='top', anchor='center', fill='none', padx=px, pady=py)
+
+    def logo(self, arquivo, px=5, py=2, bg='#dde'):
+        logo = tk.Label(self, image=arquivo, background=bg)
+        logo.pack(side='left', anchor='nw', fill='none', padx=px, pady=py)
+
+    def status(self, bg='#dde'):
+        status = tk.Label(self, text='teste status', background=bg)
+        status.pack(side='right', anchor='ne', fill='none', padx=2, pady=2)
+
+    def botoes(self, px=35, py=5):
+        internacao = tk.Button(self, text='Internação', command=abre_janela_internacao)
+        internacao.pack(side='bottom', anchor='sw', fill='none', padx=px, pady=py)
+
+
+menu = JanelaMenu()
+
+menu.fundo()
+menu.botoes()
+arquivo_logo = tk.PhotoImage(file='logo_unimed_init.png')
+menu.logo(arquivo_logo)
+menu.status()
+menu.titulos('Unimed Noroeste Capixaba', 'Senso Hospitalar', 'Ocupação Diária dos Leitos')
+
+menu.mainloop()
